dd3ef9b48e52e492b098443439cc60c467acd464
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-11 06:05:52 +0900

    Update DATABASES setting to use MySQL with environment variables

--- note/note/settings.py
+++ note/note/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'note.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME', default='note'),  # 데이터베이스 이름
+        'USER': config('DB_USER', default='root'),  # MySQL 사용자 이름
+        'PASSWORD': config('DB_PASSWORD'),  # MySQL 비밀번호
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
+
 
 
 # Password validation
