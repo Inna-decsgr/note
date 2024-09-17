a757a0eb28e0707c0afe29bffd2d361dc3f8b76a
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-17 22:52:07 +0900

    Update HomeView component to display memo list from API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>메모 목록</h2>
+    <ul v-if="memos.length">
+      <li v-for="memo in memos" :key="memo.id">
+        <h3>{{ memo.title }}</h3>
+        <p>{{ memo.content }}</p>
+      </li>
+    </ul>
+    <p v-else>작성된 메모가 없습니다.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      memos: []
+    };
+  },
+  async created() {
+    await this.fetchMemos();
+  },
+  methods: {
+    async fetchMemos() {
+      try {
+        const response = await axios.get('http://localhost:8000/api/memos/');
+        this.memos = response.data;
+      } catch (error) {
+        console.error('메모 목록을 가져오던 중 에러가 발생했습니다', error);
+      }
+    }
   }
 }
 </script>
