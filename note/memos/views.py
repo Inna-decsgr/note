7513e05b9c61101ed5cca07a10b0838dedfb7480
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-17 22:50:53 +0900

    Add `memo_list` view to fetch and return all memos as JSON

--- note/memos/views.py
+++ note/memos/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Memo
+from .serializers import MemoSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def memo_list(request):
+    memos = Memo.objects.all()
+    serializer = MemoSerializer(memos, many=True)
+    return Response(serializer.data)
