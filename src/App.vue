3df660b09963cb3b1c561d5e13e9712f6a9bb499
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-18 23:44:14 +0900

    Enhance layout with centered content and improved styling for navigation and container

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,53 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> | 
-    <router-link to="/create">메모 작성</router-link>
-  </nav>
-  <router-view/>
+  <div id="id" class="app-container">
+    <div class="content-wrapper">
+      <nav class="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/create">메모 작성</router-link>
+      </nav>
+    <router-view/>
+    </div>
+  </div>
 </template>
 
 <style>
-  nav {
-    margin-bottom: 20px;
-  }
+.app-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+
+.content-wrapper {
+    position: relative; /* 상대적 위치 */
+    width: 100%; 
+    max-width: 700px;
+    height: 700px;
+    padding: 35px; 
+    border: 1px solid #dee2e6; 
+    border-radius: 8px;
+    background-color: white; 
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    box-sizing: border-box;
+    overflow: auto;
+}
+
+.nav {
+  margin-bottom: 20px;
+  text-align: left;
+}
+
+.nav a {
+  color: black;
+  font-weight: bold;
+  text-decoration: none;
+  margin: 0 20px 20px 0;
+}
+
+ol, ul {
+  list-style: none;
+  padding: 0 !important;  
+  margin: 0;       
+}
 </style>
