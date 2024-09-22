12c26b857c2fead061db1b3ba3b4b98aa1fd39ab
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-23 00:10:08 +0900

    Implement logic to fetch memo data for editing and update title based on edit state

--- src/views/CreateMemo.vue
+++ src/views/CreateMemo.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="memo-wrapper">
-    <span class="title">새 메모 작성</span>
-    <NewMemo />
+    <span class="title">{{  isEdit ? '메모 수정하기' : '새 메모 작성' }}</span>
+    <NewMemo :existMemo="memo" :isEdit="isEdit"/>
   </div>
 </template>
 
 <script>
-import NewMemo from '@/components/NewMemo.vue'
+import NewMemo from '@/components/NewMemo.vue';
+import axios from 'axios';
 
 export default {
   components: {
     NewMemo
+  },
+  data() {
+    return {
+      memo: { title: '', content: '' },
+      isEdit: false
+    }
+  },
+  async created() {
+    const memoId = this.$route.query.memoId;
+    if (memoId) {
+      this.isEdit = true;
+      try {
+        const response = await axios.get(`http://localhost:8000/api/memos/${memoId}/`);
+        this.memo = response.data;
+      } catch (error) {
+        console.error('메모를 불러오는 중 오류 발생:', error);
+      }
+    }
   }
 }
 </script>
