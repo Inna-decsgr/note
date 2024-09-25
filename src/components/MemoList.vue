f92db2c677746dfe13ed149c5a45cef7ff77a3cb
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-25 17:24:23 +0900

    Update memo list styling and improve memo edit button UI

--- src/components/MemoList.vue
+++ src/components/MemoList.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="memo-list">
     <ul v-if="memos.length">
-      <li v-for="memo in memos" :key="memo.id">
-        <span class="memo-title">{{ memo.title }}</span>
-        <p class="memo-content">{{ memo.content }}</p>
-        <button @click="editmemo(memo)">메모 수정</button>
+      <li v-for="memo in memos" :key="memo.id" class="d-flex justify-content-between align-items-center">
+        <div class="flex-grow-1 me-3">
+          <span class="memo-title">{{ memo.title }}</span>
+          <p class="memo-content">{{ memo.content }}</p>
+        </div>
+        <button @click="editmemo(memo)" class="btn btn-secondary" style="width: 100px;  line-height:1 ;">메모 수정</button>
       </li>
     </ul>
     <p v-else>작성된 메모가 없습니다.</p>
@@ -21,7 +23,7 @@ export default {
   },
   methods: {
     editmemo(memo) {
-      // memo 데이터를 가지고 /create 경로로 이동
+      // 수정 버튼을 클릭하면 memo 데이터를 가지고 /create 경로로 이동
       this.$router.push({
         path: '/create',
         query: { memoId: memo.id }
@@ -60,4 +62,11 @@ li {
   margin-top: 5px;
   height: 20px;
 }
+
+.memo-list .btn {
+  margin-top: 0px;
+  font-size: 12px;
+}
+
+
 </style>
