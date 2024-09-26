45a41d27c4364e7811b1115c1a6382296dbea514
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-26 13:51:54 +0900

    Implement memo deletion feature with user confirmation and update the memo list accordingly

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <span class="title">메모 목록</span>
-    <MemoList :memos="memos" />
+    <MemoList :memos="memos"  @delete-memo="deleteMemo"/>
   </div>
 </template>
 
@@ -28,6 +28,28 @@ export default {
         this.memos = response.data;
       } catch (error) {
         console.error('메모 목록을 가져오던 중 에러가 발생했습니다', error);
+      }
+    },
+    async deleteMemo(memoId) {
+      // 사용자에게 확인을 요청하는 confirm() 함수 사용
+      const isConfirmed = confirm('메모를 정말 삭제하시겠습니까?');
+      if (isConfirmed) {
+        try {
+          // 확인을 누르면 실행된 메모 삭제 로직 추가
+          const response = await axios.delete(`http://localhost:8000/api/memos/${memoId}/`);
+          console.log('메모 삭제 성공:', response.data);
+
+          // 여기서 삭제 후 필요한 후처리(예: 메모 리스트 갱신)을 해줄 수 있다.
+          // 필터링해서 memoId와 일치하지 않는 메모들만 memos에 담기 = 삭제할 메모 외의 메모들만 담긴다
+          // 이때 상태 업데이트를 부모 컴포넌트에서 수행한다. 
+          // props로 전달된 데이터를 자식 컴포넌트에서는 변경시킬 수 없기 때문에 삭제 로직은 부모 컴포넌트에서 작성한다.
+          // 작성한 로직을 자식 컴포넌트에게 전달하게 되면 자식 컴포넌트에서는 해당 로직이 필요한 시점에 emit을 통해 부모 컴포넌트의 함수를 호출해서 상태를 변경시킨다.
+          this.memos = this.memos.filter(memo => memo.id !== memoId);
+        } catch (error) {
+          console.error('메모 삭제 중 오류 발생:', error);
+        }
+      } else {
+        console.log('메모 삭제 취소');
       }
     }
   }
