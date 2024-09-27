ea9efcb7cc77fc2ea84b7c33b41313ed5c3a7cdd
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-27 21:29:24 +0900

    Add boolean field is_bookmark to track bookmarked memos

--- note/memos/models.py
+++ note/memos/models.py
@@ -4,6 +4,7 @@ class Memo(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    is_bookmark = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
