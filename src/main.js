399d4655851a22f6f89cfc26a2bbea5f0cbede95
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-28 19:12:53 +0900

    Add FontAwesome icons for favorite feature

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+
 
 // Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js' 
 
-createApp(App).use(router).mount('#app')
+// 라이브러리에 아이콘을 추가해야 해
+library.add(fasStar, farStar);
+
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .use(router)
+  .mount('#app')
