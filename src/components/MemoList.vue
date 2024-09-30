91247ca8b27c6759351ccf966f0d4b0a6923a90d
Inna-decsgr <anjgkwl27@gmail.com>
2024-09-30 14:58:50 +0900

    Enhance memo list with date formatting and bookmark toggle feature

--- src/components/MemoList.vue
+++ src/components/MemoList.vue
@@ -5,9 +5,13 @@
         <div class="flex-grow-1 me-3">
           <span class="memo-title">{{ memo.title }}</span>
           <p class="memo-content">{{ memo.content }}</p>
+          <span class="memo-date">{{ formatDate(memo.created_at) }}</span>
         </div>
-        <button @click="editmemo(memo)" class="btn btn-secondary" style="width: 100px;  line-height:1; margin-right: 4px">메모 수정</button>
-        <button @click="deletememo(memo.id)" class="btn btn-secondary" style="width: 100px;  line-height:1 ;">삭제</button>
+        <button @click="editmemo(memo)" class="btn btn-secondary custom-btn">메모 수정</button>
+        <button @click="deletememo(memo.id)" class="btn btn-secondary custom-btn">삭제</button>
+        <button class="starbutton btn btn-secondary custom-btn" @click="toggleBookmark(memo)">
+          <font-awesome-icon :icon="memo.is_bookmark ? 'fas fa-star' : 'far fa-star'" />
+        </button>
       </li>
     </ul>
     <p v-else>작성된 메모가 없습니다.</p>
@@ -16,6 +20,9 @@
 
 
 <script>
+import axios from 'axios';
+import { formatDate } from '@/utils/format';
+
 export default {
   props: {
     memos: {
@@ -24,6 +31,9 @@ export default {
     }
   },
   methods: {
+    formatDate(createdAt) {
+      return formatDate(createdAt);
+    },
     editmemo(memo) {
       // 수정 버튼을 클릭하면 memo 데이터를 가지고 /create 경로로 이동
       this.$router.push({
@@ -35,6 +45,14 @@ export default {
     async deletememo(memoId) {
       // 삭제 요청을 부모 컴포넌트에 전달, 부모 컴포넌트의 삭제 로직 실행시키도록
       this.$emit('delete-memo', memoId);
+    },
+    async toggleBookmark(memo) {
+      try {
+        const response = await axios.post(`http://localhost:8000/api/memos/${memo.id}/toggle_bookmark/`);
+        memo.is_bookmark = response.data.is_bookmark;  // API 응답에 따라 즐겨찾기 상태 업데이트
+      } catch (error) {
+        console.error('즐겨찾기 토글 중 오류 발생:', error);
+      }
     }
   }
 }
@@ -74,5 +92,23 @@ li {
   font-size: 12px;
 }
 
+.starbutton {
+  outline: none;
+  width: 40px !important;  
+  margin-right: 0;
+}
+
+.memo-date {
+  font-size: 12px;
+  font-weight: bold;
+  color:#9b9898
+}
+
+
+.custom-btn {
+  width: 100px !important;
+  line-height: 1 !important;
+  margin-right: 5px !important;
+}
 
 </style>
