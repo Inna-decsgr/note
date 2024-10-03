6c56f63e7bfeb377db79d3296e1587e4ec08ecd8
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-03 19:48:00 +0900

    Implement bookmark toggle and add memo creation feature

--- note/memos/views.py
+++ note/memos/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from .models import Memo
 from .serializers import MemoSerializer
 from django.shortcuts import get_object_or_404
+import json
 
 @api_view(['GET'])
 def memo_list(request):
@@ -19,3 +20,30 @@ def toggle_bookmark(request, memo_id):
     memo.is_bookmark = not memo.is_bookmark  # 즐겨찾기 상태 반전
     memo.save()
     return JsonResponse({'is_bookmark': memo.is_bookmark})  # 새로운 상태 반환
+
+# 새 메모 작성
+def create_memo(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)  # JSON 데이터 로드
+        title = data.get('title')
+        content = data.get('content')
+        is_locked = data.get('is_locked', False)  # 기본값 False
+        is_bookmark = data.get('is_bookmark', False) 
+        password = data.get('password')
+
+        # 메모 생성
+        memo = Memo.objects.create(title=title, content=content, is_locked=is_locked, is_bookmark=is_bookmark, password=password)
+
+        return JsonResponse({
+            'id': memo.id, 
+            'title': memo.title, 
+            'content': memo.content, 
+            'is_locked': memo.is_locked,
+            'is_bookmark': memo.is_bookmark,
+            'created_at': memo.created_at
+        }, status=201)
+
+    return JsonResponse({'error': 'Invalid method'}, status=405)
+
+
+# 메모 조회
