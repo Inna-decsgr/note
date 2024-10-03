38017cfdc8331526493d8352432b4f40a0524f95
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-03 19:48:52 +0900

    Implement memo locking with password for secure memos

--- src/components/NewMemo.vue
+++ src/components/NewMemo.vue
@@ -19,6 +19,19 @@
         placeholder="메모 내용을 입력하세요."
       />
 
+      <label class="form-label">
+        <input type="checkbox" name="is_locked" v-model="memo.is_locked"> 잠금 여부🔐
+        <div v-if="memo.is_locked" class="password">
+          <input 
+            type="password"
+            v-model="memo.password"
+            id="password"
+            class="form-control password"
+            placeholder="메모 잠금을 위한 비밀번호를 입력하세요"
+          >
+        </div>
+      </label>
+
       <button type="submit" class="btn btn-primary">
         {{ isEdit ? '수정하기' : '저장하기' }}
       </button>
@@ -34,7 +47,7 @@ export default {
     // props로 memo 데이터를 받고, 수정 모드인지 여부도 받기
     existMemo: {
       type: Object,
-      default: () => ({ title: '', content: '' })
+      default: () => ({ title: '', content: '', is_locked: false })  // 기본값 설정
     },
     isEdit: {
       type: Boolean,
@@ -43,11 +56,15 @@ export default {
   },
   data() {
     return {
-      memo: { ...this.existMemo }  // memo를 props로 받은 axiosMemo로 초기화
+      memo: {
+        ...this.existMemo,
+        is_locked: this.existMemo.is_locked || false,
+        password: ''
+      }  // memo(기존 메모)를 props로 받은 existMemo로 초기화
     };
   },
   watch: {
-    // existMemo가 변경되면 memo 값도 업데이트
+    // existMemo가 변경되면 memo 값도 자동으로 업데이트
     existMemo(newMemo) {
       this.memo = {...newMemo}
     }
@@ -55,17 +72,22 @@ export default {
   methods: {
     async submitMemo() {
       try {
+        // 비밀번호가 잠금 상태이고 비밀번호 입력이 없으면 경고 메시지
+        if (this.memo.is_locked && !this.memo.password) {
+          alert('잠금 메모의 비밀번호 입력은 필수입니다.')
+          return;
+        }
+
         if (this.isEdit) {
           // 메모 수정하는 PUT 요청
           const response = await axios.put(`http://localhost:8000/api/memos/${this.memo.id}/`, this.memo);
           console.log('메모 수정 성공:', response);
           alert('성공적으로 메모가 수정되었습니다.')
-          this.$router.push('/');
         } else {
           // 새 메모 작성하는 POST 요청
-        const response = await axios.post('http://localhost:8000/api/memos/', this.memo);
-        console.log('새 메모 작성 성공:', response);
-        alert('성공적으로 메모가 작성되었습니다.')
+          const response = await axios.post('http://localhost:8000/api/memos/', this.memo);
+          console.log('새 메모 작성 성공:', response);
+          alert('성공적으로 메모가 작성되었습니다.')
         }
         this.$router.push('/');
       } catch (error) {
@@ -89,6 +111,7 @@ export default {
 .content {
   height: 100px;
   overflow-y: auto;
+  margin-bottom: 30px;
 }
 
 .form-control::placeholder {
@@ -100,4 +123,11 @@ export default {
   width: 100%;
   margin-top: 40px;
 }
+
+
+.password {
+  display: inline !important;
+  margin-top: 10px;
+}
+
 </style>
